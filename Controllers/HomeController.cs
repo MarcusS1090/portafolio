dec04e4ecfbaa2819c16744dd9f863512df2f58c
Marcus Aurelius Senju <thegladus323@gmail.com>
2024-11-02 21:16:36 -0500

    iniciando el apartado de la vista

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace portafolio.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult index()
         {
             return View();
         }
